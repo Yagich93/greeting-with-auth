1e95a21db5022a0c6ac91c4b3933a3e8542bfc2d
Iakov Lushin <i.lushin@draewil.com>
2020-09-28 11:11:05 +0300

    Add finding user by credentials

--- src/db/user-db.js
+++ src/db/user-db.js
@@ -7,6 +7,15 @@ class UserDb {
     const foundUser = USERS.find(user => user.token === token)
     return foundUser || null
   }
+
+  findByCredentials(credentials) {
+    const foundUser = USERS.find(user => this._doesUserMatchCredentials(user, credentials))
+    return foundUser || null
+  }
+
+  _doesUserMatchCredentials(user, credentials) {
+    return user.name === credentials.name && user.password === credentials.password
+  }
 }
 
 module.exports = { UserDb }
